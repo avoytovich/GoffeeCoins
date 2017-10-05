fb2460807821c3114b9b6f5f1c9ad2319754ba7b
Yevhen Kravets <krava2108@gmail.com>
2017-10-05 19:28:49 +0300

    asdasd

--- models/adminRequest.model.js
+++ models/adminRequest.model.js
@@ -38,15 +38,3 @@ const adminRequestSchema = new mongoose.Schema({
 const AdminRequest = mongoose.model(MODELS.ADMIN_REQUEST, adminRequestSchema);
 
 module.exports = AdminRequest;
-
-AdminRequest.create({
-    adminID: 'EoE8TKSNKcSbFJXagznOMfnAiZg1',
-    userID: 'NYJVszMLdlUgq7j8IR4KRU4WRuJ3',
-    coffeeHouseID: '59d501ef3063254f96dc5d5b',
-});
-
-AdminRequest.create({
-    adminID: 'HNTZTCbcO2cJMfckaJmLjng1mc13',
-    userID: 'NYJVszMLdlUgq7j8IR4KRU4WRuJ3',
-    coffeeHouseID: '59c9d506ce0e011b6d53d0c9',
-});

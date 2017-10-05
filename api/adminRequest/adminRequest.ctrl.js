06177874dc4ad06778472764f2dfac43b9066bd8
Yevhen Kravets <krava2108@gmail.com>
2017-10-05 19:30:03 +0300

    asdasd

--- api/adminRequest/adminRequest.ctrl.js
+++ api/adminRequest/adminRequest.ctrl.js
@@ -38,17 +38,3 @@ module.exports = {
     },
 
 };
-
-
-
-AdminRequest.create({
-    adminID: '81Q5qFP73cNET3C6tfP9T4qLyQx1',
-    userID: 'NYJVszMLdlUgq7j8IR4KRU4WRuJ3',
-    coffeeHouseID: '59d501ef3063254f96dc5d5b',
-});
-
-AdminRequest.create({
-    adminID: '81Q5qFP73cNET3C6tfP9T4qLyQx1',
-    userID: 'NYJVszMLdlUgq7j8IR4KRU4WRuJ3',
-    coffeeHouseID: '59c9d506ce0e011b6d53d0c9',
-});

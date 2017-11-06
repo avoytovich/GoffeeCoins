fdc6b8828c7de7da4728eda3b220769a90371cff
Yevhen Kravets <krava2108@gmail.com>
2017-11-06 17:51:38 +0200

    fix production notifications

--- env/development/index.js
+++ env/development/index.js
@@ -7,6 +7,7 @@ module.exports = {
     adminPort: env.ADMIN_PORT || 8031,
     secret: '9datuWxCQFWcqStAOuZC',
     mongoUrl: env.MONGO_URL || 'mongodb://localhost:27017/coffeeCoinsDev',
+    GCM: 'AAAAOqxfVd4:APA91bF4mVW1HuK2g5TtSvXvPoEavxokQO7kK_1uoCxgjMFkdfxF65EMAhPpbnShgr_Ju3C7pcwh3HMoOaA5JIqoIkSALwDGWOSAjoWO89kZNqevWRRBCpPEDKzAnJuOruCF4aPNiMSO',
     firebaseURL: 'https://dev-remotediscount.firebaseio.com',
     mailjet: {
         apiKey: "1a58a3c2244c772f1e4ed932ce8ba928",
--- env/index.js
+++ env/index.js
@@ -7,7 +7,6 @@ const envConfig = require(`./${nodeEnv}`);
 const generalConfig = {
     nodeEnv,
     isProduction: env.NODE_ENV === 'production',
-    GCM: 'AAAAOqxfVd4:APA91bF4mVW1HuK2g5TtSvXvPoEavxokQO7kK_1uoCxgjMFkdfxF65EMAhPpbnShgr_Ju3C7pcwh3HMoOaA5JIqoIkSALwDGWOSAjoWO89kZNqevWRRBCpPEDKzAnJuOruCF4aPNiMSO',
     bundleId: 'com.boostmanagement.CofeeCoin',
 };
 

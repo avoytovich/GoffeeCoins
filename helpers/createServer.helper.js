316c2f39c373ae13fb7c60bff56c680ff787fa03
Yevhen Kravets <krava2108@gmail.com>
2017-11-15 15:40:35 +0200

    fix text in notifications

--- helpers/createServer.helper.js
+++ helpers/createServer.helper.js
@@ -16,7 +16,7 @@ function createServer(port, router) {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json({limit: '10mb'}));
     app.use(require('../middleware/cors'));
-    app.use( express.static(join(process.cwd(), 'public', 'dist')));
+    app.use(express.static(join(process.cwd(), 'public', 'dist')));
     app.use('/apidoc', express.static('apidoc'));
     app.use(validatorMiddleware);
     app.use('/api/v1', router);

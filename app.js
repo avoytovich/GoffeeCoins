efccfdf719233b95c2ab8fb39b7a5ecaff09b23a
Alex Prylypko <oleksandr.prylypko@gmail.com>
2017-11-16 02:20:44 +0200

    add pug view

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const compression = require('compression');
 const helmet = require('helmet');
 const path = require('path');
 const app = express();
-const lessMiddleware = require('less-middleware');
+
 
 
 app.set('views', path.join(__dirname, 'views'));
 // view engine setup
 app.set('view engine', 'pug');
-app.use(lessMiddleware(path.join(__dirname, 'public')));
+
 
 app.use(helmet());
 app.use(compression());

714a3fff666107046f51a4fcbc1b9f6ea3217572
Andrii <andrew.voytovich@boost.solutions>
2018-01-31 16:25:40 +0200

    Replaced AWS_s3 instead of knoxConfig

--- libs/knox.js
+++ libs/knox.js
@@ -1,8 +1,13 @@
 'use strict';
 
-const { knoxConfig } = require('../env');
+const { AWS_s3 } = require('../env');
+const AWS = {
+    key: AWS_s3.Access_KEY,
+    secret: AWS_s3.Access_SECRET,
+    bucket: AWS_s3.S3_BUCKET
+};
 const knox = require('knox');
 
-const client = knox.createClient(knoxConfig);
+const client = knox.createClient(AWS);
 
 module.exports = client;
